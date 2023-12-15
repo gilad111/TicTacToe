8e6f2bca84472d5041728731c8629fd0f7522476
gilad111 <giladyativ10@gmail.com>
2023-12-15 20:51:47 +0200

    Add board game functionality of onCreate, isWinner and endGame.

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -16,11 +16,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
 
-    @Override
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.e(TAG,  "MainActivity::OnCreate");
+        onNewGame();
     }
 
     @Override
@@ -67,23 +68,47 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean isWinner()
     {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(board[i][1]) && board[i][1].equals(board[i][2]) && !board[i][0].equals("")) {
+                return true; // Row win
+            }
+            if (board[0][i].equals(board[1][i]) && board[1][i].equals(board[2][i]) && !board[0][i].equals("")) {
+                return true; // Column win
+            }
+        }
 
+        // Check diagonals
+        if (board[0][0].equals(board[1][1]) && board[1][1].equals(board[2][2]) && !board[0][0].equals("")) {
+            return true; // Diagonal win
+        }
+        if (board[0][2].equals(board[1][1]) && board[1][1].equals(board[2][0]) && !board[0][2].equals("")) {
+            return true; // Diagonal win
+        }
+
+        return false; // No win
     }
 
     private void endGame (String st)
     {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("More Info");
-        String msg = "This is the message body";
+        builder.setTitle("We have a winner!");
+        String msg = "The winner is: " +st;
+        if(st.equals("Tie"))
+        {
+            builder.setTitle("We got a tie");
+             msg = "The result is : " +st;
+        }
+
         builder.setMessage(msg);
         builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 // Exit handling
+                onDestroy();
 
             }
         });
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("Restart", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
 // Cancel handling
@@ -121,34 +146,27 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void onButtonClick(View view) {
-        switch (view.getId()) {
-            case R.id.btn_00:
-                handleClick(0, 0, R.id.btn_00);
-                break;
-            case R.id.btn_01:
-                handleClick(0, 1, R.id.btn_01);
-                break;
-            case R.id.btn_02:
-                handleClick(0, 2, R.id.btn_02);
-                break;
-            case R.id.btn_03:
-                handleClick(1, 0, R.id.btn_03);
-                break;
-            case R.id.btn_04:
-                handleClick(1, 1, R.id.btn_04);
-                break;
-            case R.id.btn_05:
-                handleClick(1, 2, R.id.btn_05);
-                break;
-            case R.id.btn_06:
-                handleClick(2, 0, R.id.btn_06);
-                break;
-            case R.id.btn_07:
-                handleClick(2, 1, R.id.btn_07);
-                break;
-            case R.id.btn_08:
-                handleClick(2, 2, R.id.btn_08);
-                break;
+        Log.e(TAG, "alert");
+        int id = view.getId();
+
+        if (id == R.id.btn_00) {
+            handleClick(0, 0, R.id.btn_00);
+        } else if (id == R.id.btn_01) {
+            handleClick(0, 1, R.id.btn_01);
+        } else if (id == R.id.btn_02) {
+            handleClick(0, 2, R.id.btn_02);
+        } else if (id == R.id.btn_10) {
+            handleClick(1, 0, R.id.btn_10);
+        } else if (id == R.id.btn_11) {
+            handleClick(1, 1, R.id.btn_11);
+        } else if (id == R.id.btn_12) {
+            handleClick(1, 2, R.id.btn_12);
+        } else if (id == R.id.btn_20) {
+            handleClick(2, 0, R.id.btn_20);
+        } else if (id == R.id.btn_21) {
+            handleClick(2, 1, R.id.btn_21);
+        } else if (id == R.id.btn_22) {
+            handleClick(2, 2, R.id.btn_22);
         }
     }
 
